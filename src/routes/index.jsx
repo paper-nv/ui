6c2a7564dd898b34ed52b72262b5c00db372103f
Henry chidebe <techchida@outlook.com>
2023-10-22 15:46:51 +0100

    bug fixes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,7 +3,7 @@ import SignIn from "../pages/signIn/SignIn";
 import Dashboard from "../pages/dashboard/Dashboard";
 import Invoices from "../pages/invoices/Invoices";
 import Profiles from "../pages/profiles/profiles";
-import Customers from "../pages/profiles/Customers";
+import Customer from "../pages/profiles/Customers";
 import Newinvoice from "../pages/invoices/new";
 import Home from "../pages/home";
 
@@ -37,7 +37,7 @@ export const appRoutes = [
   },
   {
     path: "/accounts/customers",
-    element: <Customers />,
+    element: <Customer />,
   },
   {
     path: "/home",
